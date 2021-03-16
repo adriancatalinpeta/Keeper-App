3891b606161ea566f9768ddf314f8c3c0281d19d
adriancatalinpeta <adriancatalin197@gmail.com>
2021-03-17 00:27:37 +0200

    Updates

---
+++ precache-manifest.3d230fbea1d0d00439cb01004790762a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9f6a20bbf1e91f21272ebe069d5ba88",
+    "url": "/keeper-app/index.html"
+  },
+  {
+    "revision": "f3fa5ac507983a3b476d",
+    "url": "/keeper-app/static/js/2.b6ca354c.chunk.js"
+  },
+  {
+    "revision": "359f2f5ec3f552cf43ed",
+    "url": "/keeper-app/static/js/main.f033765f.chunk.js"
+  },
+  {
+    "revision": "ed9243ad83c12b8fd98c",
+    "url": "/keeper-app/static/js/runtime-main.604284a8.js"
+  }
+]);
